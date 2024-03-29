71fc02e3dd2d91ae399e94ea8ce9de4dd3b6484d
someone <someone@someplace.com>
2024-03-30 00:32:37 +0300

    helpers compiled without errors

--- Project/part3_helpers.cpp
+++ Project/part3_helpers.cpp
@@ -1,12 +1,13 @@
 /* 046266 Compilation Methods, EE Faculty, Technion                        */
-/* part3_helpers.c - Helper functions for project part 3 - implementation  */
-#include <stdio.h>
+/* part3_helpers.cpp - Helper functions for project part 3 - implementation  */
+
 
 #include "part3_helpers.h"
 
-extern int yyparse (void);
-extern Vec_buf code_buffer; //defined in parser
+//extern int yyparse (void);
+/*extern*/ Vec_buf code_buffer; //defined in parser
 
+using namespace std;
 
 /**************************************************************************/
 /*              Class for the buffer we emit the commands to              */
@@ -31,11 +32,19 @@ void Vec_buf::print_code(ofstream& output_file){
         output_file << three_add_code << endl;
 }
 
+void Vec_buf::emit(string& str) {
+    buffer.push_back(str);
+}
+
 
 
 /**************************************************************************/
 /*                       Variables Symbol Table                           */
 /**************************************************************************/
+/**************************************************************************/
+/*          VARIABLE TABLE PER TYPE PER SCOPE - lower level               */
+/**************************************************************************/
+
 
 
 Table_per_type_block_scope::Table_per_type_block_scope(Type block_table_type): //constructor
@@ -62,7 +71,7 @@ void Table_per_type_block_scope::insert_var_to_map(string id, int offset){
     vars_in_block.insert(pair<string,int>(id,offset));
 }
 
-bool TypedVarScopeTable::type_block_scope_lookup(Var_Table_Entry& var, string id){
+bool Table_per_type_block_scope::find_in_type_block_scope(Var_Table_Entry& var, string id){
     // looks for the proper var in table, if found store in Var_Table_Entry
     auto it = vars_in_block.find(id);
     if(it != vars_in_block.end()){ // id is within the symbol table
@@ -73,65 +82,163 @@ bool TypedVarScopeTable::type_block_scope_lookup(Var_Table_Entry& var, string id
     return false;
 }
 
-void TypedVarScopeTable::reset_temp_reg(){
+void Table_per_type_block_scope::reset_temp_reg(){
     next_temp_reg = temp_start_idx;
 }
 
-void TypedVarScopeTable::store_reg_update_stack(){
+void Table_per_type_block_scope::store_reg_update_stack(){
+    string three_add_code;
     int used_regs = next_temp_reg - temp_start_idx;
     string SP_reg = SP;
     if(used_regs > 0){
         if(block_table_type == float_){
             //Cast SP to a SP_F
-            code_buffer.emit("CITOF",SP_F, SP_reg);
+            three_add_code = string("CITOF") + " " SP_F + " " + SP;
+            code_buffer.emit(three_add_code);
             SP_reg = SP_F;
         }
         //Store registers
         for (int i=0; i<used_regs; i++){
-            code_buffer.emit(string("STOR")+type_rep + " " +  type_rep + to_string(temp_start_idx + i) + " " + SP_reg + " " + to_string(i * 4));
+            three_add_code = string("STOR") + type_rep + " " + type_rep + to_string(temp_start_idx + i) + " " + SP_reg + " " + to_string(i * 4);
+            code_buffer.emit(three_add_code);
         }
         //Update SP
-        code_buffer.emit(string("ADD2I ") + SP + " " + SP + " " + to_string(used_regs * 4));
+        three_add_code = string("ADD2I") + " " + SP + " " + SP + " " + to_string(used_regs * 4);
+        code_buffer.emit(three_add_code);
     }
 }
 
-void TypedVarScopeTable::load_reg_update_stack(){
+void Table_per_type_block_scope::load_reg_update_stack(){
+    string three_add_code;
     int used_regs = next_temp_reg - temp_start_idx;
     if(used_regs > 0){
         //Update SP
-        code_buffer.emit(string("SUBTI ") + SP + " " + SP + " " + to_string(used_regs * 4));
+        three_add_code = string("SUBTI") + " " + SP + " " + SP + " " + to_string(used_regs * 4);
+        code_buffer.emit(three_add_code);
         string SP_reg = SP;
         if(block_table_type == float_){
             //Cast SP to a SP_F
-            code_buffer.emit("CITOF",SP_F, SP);
+            three_add_code = string("CITOF") + " " + SP_F + " " SP;
+            code_buffer.emit(three_add_code);
             SP_reg = SP_F;
         }
         //Store registers
         for (int i=0; i<used_regs; i++){
-            code_buffer.emit(string("LOAD")+type_rep + " " +  type_rep + to_string(temp_start_idx + i) + " " + SP_reg + " " + to_string(i * 4));
+            three_add_code = string("LOAD") + type_rep + " " + type_rep + to_string(temp_start_idx + i) + " " + SP_reg + " " + to_string(i * 4);
+            code_buffer.emit(three_add_code);
         }
     }
 }
 
 
 
+/**************************************************************************/
+/*                VARIABLE TABLE PER TYPE - middle level                  */
+/**************************************************************************/
 
 
 
+Table_block_scope::Table_block_scope(int start_offset):
+    int_table(int_), float_table(float_) {
+    stack_offset = start_offset;
+    stack_start_idx = start_offset;
+}
+
+string Table_block_scope::next_temp_reg(Type type) {
+    if (type == void_t)
+        return "-1";
+    else if (type == int_) {
+        return int_table.get_next_temp();
+    }
+    else {  //type == float_
+        return float_table.get_next_temp();
+    }
+}
+
+int Table_block_scope::insert_variable(string id, Type type) {
+    if (type == void_t)
+        return -1;
+    else if (type == int_) {
+        int_table.insert_var_to_map(id, stack_offset);
+    }
+    else {  //type == float_
+        float_table.insert_var_to_map(id, stack_offset);
+    }
+    stack_offset += 4;  //update sp by type int/float size
+    return stack_offset;
+}
+
+bool Table_block_scope::find_in_block_scope(Var_Table_Entry& var, string id) {   //returns true if variable is in one of the tables
+    return int_table.find_in_type_block_scope(var, id) || float_table.find_in_type_block_scope(var, id);
+}
+
+void Table_block_scope::reset_reg() {
+    int_table.reset_temp_reg();
+    float_table.reset_temp_reg();
+}
+
+void Table_block_scope::store_reg() {
+    int_table.store_reg_update_stack();
+    float_table.store_reg_update_stack();
+}
+
+void Table_block_scope::load_reg() {
+    int_table.load_reg_update_stack();
+    float_table.load_reg_update_stack();
+}
+
+void Table_block_scope::empty_stack() {
+    string three_add_code = string("SUBTI") + " " + SP + " " + SP + " " + to_string(stack_offset);
+    if (stack_offset > stack_start_idx) //we used a stack for this block (the stack isn't empty)
+        code_buffer.emit(three_add_code);
+}
 
 
 
+/**************************************************************************/
+/*                    VARIABLE TABLE - upper level                        */
+/**************************************************************************/
 
 
 
+void variable_table::add_block_table() {
+    int stack_offset = 0;
+    if (block_tables.size() > 0) {  //if there are tables, get the offset
+        stack_offset = block_tables.front().stack_offset;
+    }
+    Table_block_scope new_table(stack_offset);  //create new block table
+    block_tables.push_front(new_table); //push it in front of other tables, so inner blocks are precedent to outer blocks
+}
 
+void variable_table::remove_block_table() {
+    block_tables.front().empty_stack();     //first empty the stack
+    block_tables.pop_front();   //then pop the block table from vector
+    if (block_tables.size())     //if there's still a table, reset the registers for it
+        block_tables.front().reset_reg();
+}
 
+bool variable_table::is_var_exist(Var_Table_Entry& var, string id) {
+    // new table is always inserted in the front of the vector, 
+    // iterating from the head of the vector ensures the right ordering for searching
+    for (auto it = block_tables.begin(); it != block_tables.end(); it++) {
+        if (it->find_in_block_scope(var, id)) 
+            return true;    //found the variable within this table
+    }
+    return false;   //didn't find the variable in any var table from the list
+}
 
+void variable_table::store() {
+    block_tables.front().store_reg();
+}
 
+void variable_table::load() {
+    block_tables.front().load_reg();
+}
 
-
-
-
+bool variable_table::find_var_in_block(string id) {
+    Var_Table_Entry temp_var;
+    return block_tables.front().find_in_block_scope(temp_var, id);
+}
 
 
 
@@ -140,9 +247,13 @@ void TypedVarScopeTable::load_reg_update_stack(){
 /*                       Functions Symbol Table                           */
 /**************************************************************************/
 /**************************************************************************/
-/*                       FUNCTION ENTRY                                   */
+/*                          FUNCTION ENTRY                                */
 /**************************************************************************/
-Function_Table_Entry::Function_Table_Entry(Type& ret_type, string& func_id, vector<Arg_dcl_data>& func_args) : 
+
+
+
+
+Function_Table_Entry::Function_Table_Entry(Type& ret_type, string& func_id, vector<Arg_dcl_data>& func_args) :     // constructor
     def_line(-1) , ret_type(ret_type), func_id(func_id), func_args(func_args) {};
 
 string Function_Table_Entry::get_func_def_place(int caller_line){
@@ -170,19 +281,20 @@ bool Function_Table_Entry::is_matching(Type& other_ret_type, string& other_func_
 
 
 /**************************************************************************/
-/*                       FUNCTION TABLE                                   */
+/*                           FUNCTION TABLE                               */
 /**************************************************************************/
-Function_Table_Entry* FunctionTable::find_func_entry(string id){
-    auto it = Func_Table.find(id);  //map method
-    if(it != Func_Table.end()) //found the function entry
+
+Function_Table_Entry* Function_Table::find_func_entry(string id){
+    auto it = func_table.find(id);  //map method
+    if(it != func_table.end()) //found the function entry
         return &(it->second);
-    return nullptr;
+    return NULL;
 }
 
-Function_Table_Entry* insert_func_entry(Type& ret_type, string& func_id, vector<Arg_dcl_data>& func_args){
+Function_Table_Entry* Function_Table::insert_func_entry(Type& ret_type, string& func_id, vector<Arg_dcl_data>& func_args){
     Function_Table_Entry newEntry(ret_type, func_id, func_args);
-    Function_Table_Entry.insert( pair<string,Function_Table_Entry>(func_id, newEntry));
-    auto it = Func_Table.find(func_id);
+    func_table.insert( pair<string,Function_Table_Entry>(func_id, newEntry));
+    auto it = func_table.find(func_id);
     return &(it->second);
 }
 
@@ -194,13 +306,14 @@ Function_Table_Entry* insert_func_entry(Type& ret_type, string& func_id, vector<
 /**************************************************************************/
 int main(void)
 {
-    int rc;
+    //int rc;
 #if YYDEBUG
     yydebug=1;
 #endif
-    rc = yyparse();
-    if (rc == 0) { // Parsed successfully
-        dumpParseTree();
-    }
-    return rc;
+    //rc = yyparse();
+    //if (rc == 0) { // Parsed successfully
+    //    dumpParseTree();
+    //}
+    //return rc;
+    return 0;
 }
--- Project/part3_helpers.h
+++ Project/part3_helpers.h
@@ -1,15 +1,23 @@
 /* 046266 Compilation Methods, EE Faculty, Technion                        */
 /* part3_helpers.h - Helper functions for project part 2 - API definitions */
 
-#ifndef COMMON_H
-#define COMMON_H
+#ifndef _PART3_HELPERS_H_
+#define _PART3_HELPERS_H_
+#include <stdio.h>
+#include <iostream>
 #include <stdlib.h>
-#include<vector>
-#include<string>
+#include <vector>
+#include <string>
+#include <map>
+#include <list>
+#include <fstream>
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+using namespace std;
+
+#define YYSTYPE Node // changed yylval type to Node
+//#ifdef __cplusplus
+//extern "C" {
+//#endif
 
 //Defining our primitive types
 typedef enum {
@@ -149,34 +157,60 @@ struct Var_Table_Entry{
     int offset;     
 };
 
-class Table_per_type_block_scope{
+class Table_per_type_block_scope {   //lower level table
 public:
     int next_temp_reg;
     int temp_start_idx;
     Type block_table_type;
     string type_rep;
-    map<string,int> vars_in_block;
+    map<string, int> vars_in_block;
 
-    Table_per_type_block_scope(Type block_table_type);
+    Table_per_type_block_scope(Type block_table_type);  //constructor
     string get_next_temp();
     void insert_var_to_map(string id, int offset);
-    bool type_block_scope_lookup(Var_Table_Entry& var, string id);
+    bool find_in_type_block_scope(Var_Table_Entry& var, string id);
     void reset_temp_reg();
     void store_reg_update_stack();
     void load_reg_update_stack();
-}
+};
 
+class Table_block_scope {   //middle level table
+public:
+    int stack_offset;
+    int stack_start_idx;
+    Table_per_type_block_scope int_table;
+    Table_per_type_block_scope float_table;
 
+    Table_block_scope(int start_offset);    //constructor gets start offset in stack
+    string next_temp_reg(Type type);        //uses get_next_temp of lower class "table per type" to get the next available reg
+    int insert_variable(string id, Type type);  //uses insert_var_to_map of lower class "table per type" with the given type
+    bool find_in_block_scope(Var_Table_Entry& var, string id);   //uses find_in_type_block_scope of lower class "table per type"
+    void reset_reg();   //uses reset_temp_reg of lower class "table per type" for both types
+    void store_reg();   //uses store_reg_update_stack of lower class "table per type" for both types
+    void load_reg();   //uses store_reg_update_stack of lower class "table per type" for both types
+    void empty_stack();
+};
 
+class variable_table {  //upper level table
+public:
+    list<Table_block_scope> block_tables;
+    list<Arg_dcl_data> func_args;
 
+    void add_block_table();
+    void remove_block_table();
+    bool is_var_exist(Var_Table_Entry& var, string id); //search for var in the tables
+    void store();
+    void load();
+    bool find_var_in_block(string id);
+};
 
 /**************************************************************************/
 /*                       Functions Symbol Table                           */
 /**************************************************************************/
 
 
-class Function_Table_Entry{
-    public:
+class Function_Table_Entry {
+public:
     int def_line;
     Type ret_type;
     string func_id;
@@ -188,69 +222,62 @@ class Function_Table_Entry{
     void define_and_backpatch(int func_def_line);
     bool is_matching(Type& other_ret_type, string& other_func_id, vector<Arg_dcl_data>& other_func_args);
 
-}
+};
 
-class FunctionTable{
+class Function_Table {
 public:
-    map<string,Function_Table_Entry> Func_Table;
+    map<string, Function_Table_Entry> func_table;
     Function_Table_Entry* curr_func_entry;
 
     Function_Table_Entry* find_func_entry(string id);
-    Function_Table_Entry* insert_func_entry(Type& ret_type, string& func_id, vector<Arg_dcl_data>& func_args); 
+    Function_Table_Entry* insert_func_entry(Type& ret_type, string& func_id, vector<Arg_dcl_data>& func_args);
 
 
-}
-
-
-
-
-
-
-
-
-
-
-
+};
 
 
 
 /**************************************************************************/
 /*              Class for the buffer we emit the commands to              */
 /**************************************************************************/
-class Vec_buf {
-private:
-    vector<string> buffer;
-    // this function is used to concatenate strings to one another with whitespace in between
-    template<typename T>
-    string concatenate_str(T t) {   //this is the base case for the recursion
-        return string(t);
-    }
 
-    template<typename T, typename... More_args>
-    string concatenate_str(T t, More_args... more_args) {
-        return string(t) + " " + concatenate_str(more_args...);     //recursive concat of input
-    }
+
+
+class Vec_buf {
+
+    // this function is used to concatenate strings to one another with whitespace in between
+    //template<typename T>
+    //string concatenate_str(T t) {   //this is the base case for the recursion
+    //    return string(t);
+    //}
+
+    //template<typename T, typename... More_args>
+    //string concatenate_str(T t, More_args... more_args) {
+    //    return string(t) + " " + concatenate_str(more_args...);     //recursive concat of input
+    //}
 
 public:
-    Vec_buf();
+    vector<string> buffer;
     int nextquad();
+
+    Vec_buf();
     void backpatch(vector<int> commitment_list, int line_number);
     void print_code(ofstream& output_file);
     //emit uses template therefore must be implemented here
-    template<typename... Args_to_emit>
-    int emit(Args_to_emit... args_to_emit){
-        string threeaddcode = concatenate_str(args_to_emit);  
-        buffer.push_back(threeaddcode);
-        return buffer.size();   //returns the next free line
-    }
+    //template<typename... Args_to_emit>
+    //int emit(Args_to_emit... args_to_emit){
+    //    string threeaddcode = concatenate_str(args_to_emit...);  
+    //    buffer.push_back(threeaddcode);
+    //    return buffer.size();   //returns the next free line
+    //}
+    void emit(string& command);   //sorry nadav it's too complicated to use the above emit
 };
 
 
-#define YYSTYPE ParserNode* // changed yylval type to ParserNode*
 
 
-#ifdef __cplusplus
-} // extern "C"
-#endif
+//#ifdef __cplusplus
+//} // extern "C"
+//#endif
 
-#endif //COMMON_H
+#endif //_PART3_HELPERS_H_
